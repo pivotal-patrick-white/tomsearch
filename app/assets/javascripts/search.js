b9e99c465e401cfcc9b6648175bee8fe0f88c184
pivotal-patrick-white <pwhite@pivotallabs.com>
2014-01-07 11:50:21 -0500

    First version

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,9 +1,19 @@
-$("<img/>")[0].src = "/assets/spinner.gif";
+img = new Image();
+img.src = "/assets/spinner.gif";
+img.src = "/assets/fresh.png";
+img.src = "/assets/rotten.png";
 
-function search(){
-	queryURI = encodeURIComponent($("#query").val())
-	$("#results").html("<img src='assets/spinner.gif'/>")
-	$.ajax({url:"/search/getMovies", data:{query:queryURI}}).done(function(data){
+$(document).ready(function(){
+	$("#searchform").submit(function(event){
+		search(1);
+		return false;
+	});
+});
+
+function search(pageno){
+	queryURI = encodeURIComponent($("#query").val());
+	$("#results").html("<div class='spinner'><img src='assets/spinner.gif'/></div>");
+	$.ajax({url:"/search/getMovies", data:{query:queryURI, page:pageno}}).done(function(data){
 		$("#results").html(data);
-        })
-}
+        });
+};
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,16 +9,18 @@ class SearchController < ApplicationController
   respond_to :json
 
   def getMovies
-    querytext = params[:query]
-    queryURI = URI.parse("http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=#{querytext}page_limit=10&page=1&apikey=7er6em5vc84hq6my9kr3t6ga")
+    @querytext = params[:query]
+    @page = params[:page].to_i
+    queryURI = URI.parse("http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=#{@querytext}&page_limit=10&page=#{@page}&apikey=7er6em5vc84hq6my9kr3t6ga")
     http = Net::HTTP.new(queryURI.host, queryURI.port)
     request = Net::HTTP::Get.new(queryURI.request_uri)
     response = http.request(request)
 
     data = response.body
     data = inflate(data) if response["Content-Encoding"] == "gzip"
-
-    render :text => JSON.parse(data)
+    
+    @total = JSON.parse(data)["total"]
+    @movies = JSON.parse(data)["movies"]
   end
 
   def inflate(string)
