3f99a365fde999b0edac8ac9b19aac732581b9b4
pivotal-patrick-white <pwhite@pivotallabs.com>
2014-01-09 17:20:02 -0500

    Changing display of cast, adding different pages

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,34 +1,77 @@
 img = new Image();
-img.src = "/assets/spinner.gif";
+//img.src = "/assets/spinner.gif";
 img.src = "/assets/fresh.png";
 img.src = "/assets/rotten.png";
-
+img.src = "/assets/cfresh.png";
 
 $(document).ready(function(){
 	$("#searchform").submit(function(event){
-		search(1);
+		search(4, 1);
 		return false;
 	});
 	$("#results").on('hidden.bs.collapse', collapsed);
-	search(1);
+	search(0, 1);
 });
 
 function collapsed(){
 	setTimeout(function(){showResults()}, 100);
 }
 
-function search(pageno){
-        queryURI = encodeURIComponent($("#query").val());
-	if(queryURI == "")
-		setTitle("Box Office");
-	else
-		setTitle("Search Movies");
+currenttype = -1;
+currentpage = -1;
+
+function search(type, pageno){
+	queryURI = encodeURIComponent($("#query").val());
+	if(type == currenttype && currentpage == pageno & type != 4){
+		return;
+	}
+	
+	if(currenttype != type){
+		if(currenttype == 4){
+			$("#searchform").collapse('hide');
+		}
+		$("#nav-bo").removeClass();
+		$("#nav-it").removeClass();
+		$("#nav-op").removeClass();
+		$("#nav-up").removeClass();
+		$("#nav-se").removeClass();
+
+		switch(type){
+			case 0:
+				setTitle("Box Office");
+				$("#nav-bo").addClass("active");
+				break;
+			case 1:
+				setTitle("In Theatres");
+				$("#nav-it").addClass("active");
+				break;
+			case 2:
+				setTitle("Opening Movies");
+				$("#nav-op").addClass("active");
+				break;
+			case 3:
+				setTitle("Upcoming Movies");
+				$("#nav-up").addClass("active");
+				break;
+			case 4:
+				setTitle("Search Movies");
+				$("#nav-se").addClass("active");
+				$("#searchform").collapse('show');
+				break;
+		}
+	}
+	currentpage = pageno;
+	currenttype = type;
+
 	$("#results").collapse('hide');
-        $("#spinner").html("<div class='spinner'><img src='assets/spinner.gif'/></div>");
-        $.ajax({url:"/search/getMovies", data:{query:queryURI, page:pageno}}).done(function(data){
+	//$("#spinner").html("<div class='spinner'><img src='assets/spinner.gif'/></div>");
+	if(type == 4 && queryURI == ""){
+		return;
+	}
+	$.ajax({url:"/search/getMovies", data:{query:queryURI, page:pageno, resulttype:type}}).done(function(data){
 		resultdata = data;
 		showResults();
-        });
+	});
 };
 
 resultsready = 0;
@@ -37,7 +80,7 @@ resultdata = "";
 function showResults(){
 	resultsready++;
 	if(resultsready == 2) {
-		$("#spinner").html("");
+		//$("#spinner").html("");
 		$("#results").html(resultdata);
 		$("#results").collapse('show');
 		resultsready = 0;
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,12 +9,22 @@ class SearchController < ApplicationController
   end
 
   def getMovies
+		@resulttype = params[:resulttype].to_i
     @querytext = params[:query]
     @page = params[:page].to_i
-    queryURI = URI.parse("http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=#{@querytext}&page_limit=10&page=#{@page}&apikey=7er6em5vc84hq6my9kr3t6ga")
-    if @querytext == ""
-      @page = -1
-      queryURI = URI.parse("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=16&country=us&apikey=7er6em5vc84hq6my9kr3t6ga")
+		case @resulttype
+		when 0
+      queryURI = URI.parse("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=10&country=us&apikey=7er6em5vc84hq6my9kr3t6ga")
+			@page = -1
+		when 1
+			queryURI = URI.parse("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?page_limit=10&page=#{@page}&country=us&apikey=7er6em5vc84hq6my9kr3t6ga")
+		when 2
+			queryURI = URI.parse("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/opening.json?limit=16&country=us&apikey=7er6em5vc84hq6my9kr3t6ga")
+			@page = -1
+		when 3
+			queryURI = URI.parse("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json?page_limit=10&page=#{@page}&country=us&apikey=7er6em5vc84hq6my9kr3t6ga");
+		when 4
+    	queryURI = URI.parse("http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=#{@querytext}&page_limit=10&page=#{@page}&apikey=7er6em5vc84hq6my9kr3t6ga")
     end
     http = Net::HTTP.new(queryURI.host, queryURI.port)
     request = Net::HTTP::Get.new(queryURI.request_uri)
